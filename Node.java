9dea14bf068c0ee9439cacee4867bd67181ec15e
pranavv01 <anand.pranav26@gmail.com>
2026-05-11 17:45:04 +0530

    creation of parents, childrens.

--- Node.java
+++ Node.java
@@ -22,12 +22,81 @@ class Pranav {
             System.out.println(node.data + "is NULL");
         }
         else {
-            System.err.println(node.data + " is " + parent.data);
+            System.out.println(node.data + " is " + parent.data);
         }
         
         for (Node child : node.children){
             printParents(child, node);
         }
+    }
+
+    static void printChildren(Node node) {
+        System.out.println(node.data + " is ");
+        for (Node child : node.children){
+            System.out.println(child.data + " ");
+        }
+        
+        System.out.println();
+
+        for (Node child : node.children) {
+            printChildren(child);
+        }
+    }
+
+    static void printLeafNodes(Node node) {
+        if(node.children.isEmpty()) {
+            System.out.println(node.data + " ");
+            return;
+        }
+
+        for (Node child : node.children) {
+            printLeafNodes(child);
+        }
+    }
+
+    static void printDegrees(Node node , Node parent) {
+        int degree = node.children.size();
+        if(parent != null) {
+            degree++;
+        }
+             System.out.println(node.data + " is " + degree);
+
+        for (Node child : node.children)
+        {
+            printDegrees(child, node);
+
+        }
+    }
+
+    public static void main (String [] args) {
+        //  Creating nodes
+
+        Node root  = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+
+
+        // Constructing tree
+        addChild(root, n2);
+        addChild(root, n3);
+        addChild(n2, n4);
+        addChild(n2, n5);
+
+        System.out.println("Parent of each node: ");
+        printParents(root, null);
+        
+        System.out.println("Children of each node: ");
+        printChildren(root);
+        
+        System.out.println("Leaf Nodes: " );
+        printLeafNodes(root);
+        System.out.println();
+
+        System.out.println("Degrees of nodes: ");
+        printDegrees(root, null);
+
     }
 
 }
